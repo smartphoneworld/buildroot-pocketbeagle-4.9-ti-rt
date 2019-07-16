f5642e61f490a28016e63b28e6db46a39fc6a07f
Mahammad <cairo74@gmail.com>
2019-07-16 04:37:27 +0200

    downgrade opencv to 3.2

--- package/opencv3/opencv3.mk
+++ package/opencv3/opencv3.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-OPENCV3_VERSION = 3.4.6
+#OPENCV3_VERSION = 3.4.6
+OPENCV3_VERSION = 3.2.0
 OPENCV3_SITE = $(call github,opencv,opencv,$(OPENCV3_VERSION))
 OPENCV3_INSTALL_STAGING = YES
 OPENCV3_LICENSE = BSD-3-Clause
